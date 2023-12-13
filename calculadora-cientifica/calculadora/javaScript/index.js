23da418e0abb154906112bd0eea2a02e248bba7c
Wanderson Gustavo da Silva <62720366+WandersonGustavo@users.noreply.github.com>
2023-12-13 16:33:42 -0300

    atualização

--- calculadora-cientifica/calculadora/javaScript/index.js
+++ calculadora-cientifica/calculadora/javaScript/index.js
@@ -1,49 +1,64 @@
-import{soma} from "../javaScript/math/soma.js" 
-import{subtracao} from "../javaScript/math/subtracao.js" 
-import{multiplicacao} from "../javaScript/math/multiplicacao.js" 
+import { divisao } from "./math/divisao.js";
+import { fatorial } from "./math/fatorial.js";
+import { media } from "./math/media.js";
+import { multiplicacao } from "./math/multiplicacao.js";
+import { porcentagem } from "./math/porcentagem.js";
+import { potencia } from "./math/potencia.js";
+import { raizQuadrada } from "./math/raizQ.js";
+import { soma } from "./math/soma.js";
+import { subtracao } from "./math/subtracao.js";       
 
-import{divisao} from "../javaScript/math/divisao.js" 
-import{raizQ} from "../javaScript/math/raizQ.js" 
-import{media} from "../javaScript/math/media.js" 
+export class App {
+    static initialize(document) {
 
-import{potencia} from "../javaScript/math/potencia.js" 
-import{fatorial} from "../javaScript/math/fatorial.js" 
-import{porcentagem} from "../javaScript/math/porcentagem.js" 
+        const btnCalcular = document.getElementById("btn-calcular");
+        const num1 = document.getElementById("num1");
+        const num2 = document.getElementById("num2");
 
-export class App{
-    static initialize(document){
+        const resultadoSoma = document.getElementById("resultado-soma");
+        const resultadoSomaInstead = document.getElementById(
+            "resultado-soma-instead"
+        );
 
-        const btnCalcular= document.getElementById("btn-calcular");
-        const num1 =document.getElementById("num1");
-        const num2 =document.getElementById("num2");
+        const resultadoSubtracao = document.getElementById("resultado-subtracao");
+        const resultadoSubtracaoInstead = document.getElementById(
+            "resultado-subtracao-instead"
+        );
 
-        const resultadoSoma= document.getElementById("resultado-soma");
-        const resultadoSomaInstead= document.getElementById("resultado-soma-instead");
+        const resultadoMultiplicacao = document.getElementById("resultado-multiplicacao");
+        const resultadoMultiplicacaoInstead = document.getElementById(
+            "resultado-multiplicacao-instead"
+        );
 
-        const resultadoSubtracao= document.getElementById("resultado-subtracao");
-        const resultadoSubtracaoInstead= document.getElementById("resultado-subtracao-instead");
+        const resultadoDivisao = document.getElementById("resultado-divisao");
+        const resultadoDivisaoInstead = document.getElementById(
+            "resultado-divisao-instead"
+        );
 
-        const resultadoMultiplicacao= document.getElementById("resultado-multiplicacao");
-        const resultadoMultiplicacaoInstead= document.getElementById("resultado-multiplicacao-instead");
+        const resultadoPorcentagem = document.getElementById("resultado-porcentagem");
+        const resultadoPorcentagemInstead = document.getElementById(
+            "resultado-porcentagem-instead"
+        );
 
-        const resultadoDivisao= document.getElementById("resultado-divisao");
-        const resultadoDivisaoInstead= document.getElementById("resultado-divisao-instead");
+        const resultadoFatorial = document.getElementById("resultado-fatorial");
+        const resultadoFatorialInstead = document.getElementById(
+            "resultado-fatorial-instead"
+        );
 
-        const resultadoPotencia= document.getElementById("resultado-potencia");
-        const resultadoPotenciaInstead= document.getElementById("resultado-potencia-instead");
+        const resultadoRaizQ = document.getElementById("resultado-raizQ");
+        const resultadoRaizQInstead = document.getElementById(
+            "resultado-raizQ-instead"
+        );
 
-        const resultadoRaizQ= document.getElementById("resultado-raizQ");
-        const resultadoRaizQInstead= document.getElementById("resultado-raizQ-instead");
+        const resultadoPotencia = document.getElementById("resultado-potencia");
+        const resultadoPotenciaInstead = document.getElementById(
+            "resultado-potencia-instead"
+        );
 
-        const resultadoMedia= document.getElementById("resultado-media");
-        const resultadoMediaInstead= document.getElementById("resultado-media-instead");
-
-        const resultadoPorcentagem= document.getElementById("resultado-porcentagem");
-        const resultadoPorcentagemInstead= document.getElementById("resultado-porcentagem-instead");
-
-        const resultadoFatorial= document.getElementById("resultado-fatorial");
-        const resultadoFatorialInstead= document.getElementById("resultado-fatorial-instead");
-
+        const resultadoMedia = document.getElementById("resultado-media");
+        const resultadoMediaInstead = document.getElementById(
+            "resultado-media-instead"
+        );
 
         btnCalcular.addEventListener("click", () => {
 
@@ -73,6 +88,9 @@ export class App{
 
             resultadoSubtracao.innerHTML = subtracao(num1.value, num2.value);
             resultadoSubtracaoInstead.innerHTML = subtracao(num2.value, num1.value);
+            
+        });
+    }
             
         });
 
